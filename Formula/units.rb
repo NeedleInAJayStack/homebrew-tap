50dfe8b91744a7a4ac5361f292267127773e8485
Jay Herron <NeedleInAJayStack@protonmail.com>
2025-05-10 01:50:34 -0600

    fix: Fixes link post-publish

--- Formula/units.rb
+++ Formula/units.rb
@@ -7,7 +7,7 @@ class Units < Formula
   version "0.1.5"
 
   on_macos do
-    url "https://github.com/NeedleInAJayStack/Units/releases/download/untagged-750138ff46e97ab71f60/v0.1.5-macos-latest.tar.gz"
+    url "https://github.com/NeedleInAJayStack/Units/releases/download/v0.1.5/v0.1.5-macos-latest.tar.gz"
     sha256 "0c72165d849c8cfa43e6ea41e27d8b52dc3db4fb3e5533b275ff7076619822ca"
 
     def install
@@ -16,7 +16,7 @@ class Units < Formula
   end
 
   on_linux do
-    url "https://github.com/NeedleInAJayStack/Units/releases/download/untagged-750138ff46e97ab71f60/v0.1.5-ubuntu-latest.tar.gz"
+    url "https://github.com/NeedleInAJayStack/Units/releases/download/v0.1.5/v0.1.5-ubuntu-latest.tar.gz"
     sha256 "efb1b32e5b6fc1e6082fb892aa0acc549aeaccc72dc73d7aa6c61b7bebccf9a3"
 
     def install
